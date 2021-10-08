05b1d6c729297c916f70041a0fa22a60785910ea
tlareg <tlaregaivir@gmail.com>
2021-10-08 22:51:27 +0200

    WIP

--- src/components/TodoList/TodoList.test.js
+++ src/components/TodoList/TodoList.test.js
@@ -6,10 +6,28 @@ import { TodoList } from "./TodoList";
 describe("TodoList", () => {
   test("renders TodoList component", async () => {
     render(<TodoList />);
-
-    const reactTestingTodoItem = await screen.findByText('use React Testing Library');
-    expect(reactTestingTodoItem).toBeInTheDocument();
-
-    screen.debug();
+    // screen.debug();
   });
+
+  test("'use React Testing Library' item should be present after items loaded", async () => {
+    render(<TodoList />);
+
+    const reactTestingTodoItem = await screen.findByText(
+      /use React Testing Library/
+    );
+    expect(reactTestingTodoItem).toBeInTheDocument();
+  });
+
+  test("'use React Testing Library' item initially should NOT be completed", async () => {
+    render(<TodoList />);
+
+    const reactTestingTodoItem = await screen.findByText(
+      /use React Testing Library/
+    );
+    expect(reactTestingTodoItem).not.toHaveClass("todo-item--completed");
+  });
+
+
+  // TODO: add prettier, eslint
+  // TODO: test toggling item, test toggling item 2 times
 });
