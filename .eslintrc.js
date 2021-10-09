89735e7c065439afb6710c216dc3ab20e52a9b5b
tlareg <tlaregaivir@gmail.com>
2021-10-09 16:51:26 +0200

    setup cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,9 @@ module.exports = {
     es2021: true,
     node: true,
     'jest/globals': true,
+    'cypress/globals': true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'prettier',
-    'plugin:jest/all',
-    'plugin:jest-dom/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -19,11 +14,20 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react', 'jest', 'testing-library', 'jest-dom'],
+  plugins: ['react', 'jest', 'testing-library', 'jest-dom', 'cypress'],
   overrides: [
     {
       files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-      extends: ['plugin:testing-library/react'],
+      excludedFiles: 'cypress/**/*',
+      extends: [
+        'plugin:jest/all',
+        'plugin:jest-dom/recommended',
+        'plugin:testing-library/react',
+      ],
+    },
+    {
+      files: ['cypres/**/*.(spec|test).[jt]s'],
+      extends: ['plugin:cypress/recommended'],
     },
   ],
   rules: {},
---
+++ cypress/integration/todoList.spec.js
@@ -0,0 +1,7 @@
+describe('example to-do app', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('shows todo list', () => {});
+});
