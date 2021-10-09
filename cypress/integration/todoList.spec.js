d83d02d68136fcecb2ba29dc87e2126c2a7255fa
tlareg <tlaregaivir@gmail.com>
2021-10-09 18:19:19 +0200

    add cypress tests

--- cypress/integration/todoList.spec.js
+++ cypress/integration/todoList.spec.js
@@ -1,7 +1,38 @@
-describe('example to-do app', () => {
+describe('Todo List Page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/');
+    cy.visit('/');
   });
 
-  it('shows todo list', () => {});
+  it('shows header', () => {
+    cy.get('h1').should('contain', 'React Testing Demo');
+  });
+
+  it('shows TODO list with 4 items', () => {
+    cy.get('ul > li').should('have.length', 4);
+  });
+
+  it('should have "code Todo list logic" item completed', () => {
+    cy.get('[data-testid="code Todo list logic"]').should(
+      'have.class',
+      'todo-item--completed'
+    );
+  });
+
+  it('should have "use Cypress" item NOT completed', () => {
+    cy.get('[data-testid="use Cypress"]').should(
+      'not.have.class',
+      'todo-item--completed'
+    );
+  });
+
+  it('after click, should have "use Cypress" item completed', () => {
+    cy.get('[data-testid="use Cypress"]')
+      .click()
+      .then(() => {
+        cy.get('[data-testid="use Cypress"]').should(
+          'have.class',
+          'todo-item--completed'
+        );
+      });
+  });
 });
